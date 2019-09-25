8a785c5d8403fc8ffb066d362d730bca8ecafef0
saltpoject <saltproject.agency@gmail.com>
2019-09-25 10:54:14 +0700

    Penambahan folder Admin untuk Admin Panel

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Admin Panel Page</title>
+</head>
+<body>
+	<?php echo '<p>Hello World</p>'; 
+	      echo '<a href="../index.html">Kembali</a>
+	'?> 
+</body>
+</html>
